7751b70d2d3e24ebe20822054e17d95f880de23a
Marty McKnife <58376595+MartyMcKnife@users.noreply.github.com>
2021-12-09 11:45:02 +0800

    Made images appear in the initial page

--- pages/api/photos.ts
+++ pages/api/photos.ts
@@ -1,12 +1,14 @@
 import { Unsplash } from "../../interfaces/unsplash";
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
+import { db } from "../../utils/firebase/admin";
+import { User } from "../../interfaces/firestore";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const count = req.query.count || 30;
+  const count = parseInt((req.query.count as string) || "30", 10);
   try {
     const { data } = await axios.get<Unsplash[]>(
       "https://api.unsplash.com/photos/random",
@@ -17,19 +19,55 @@ export default async function handler(
         },
       }
     );
-    //TODO: Add our own photos to response
-    res.status(200).json(
-      data.map((image) => {
-        return {
-          link: image.urls.small,
-          width: image.width,
-          height: image.height,
-          author: image.user.name,
-          name: image.description,
-        };
-      })
+
+    const unsplashPhotos = data.map((photo) => {
+      return {
+        link: photo.urls.small,
+        width: photo.width,
+        height: photo.height,
+        name: photo.description,
+        id: photo.id,
+        author: photo.user.username,
+      };
+    });
+
+    //Retreive uploaded photos
+    const users = (await db.collection("users").get()).docs.map(
+      (user) => user.data() as User
     );
+
+    //Extract all the photos
+    const ourPhotos = users
+      .map((user) =>
+        user.photos.map((photo) => {
+          return {
+            link: photo.link,
+            width: photo.width,
+            height: photo.height,
+            name: photo.name,
+            id: photo.id,
+            author: user.username,
+          };
+        })
+      )
+      .flat();
+
+    //Combine all photos from the unsplash api and the firestore collection, only if the count is more than 20
+    //Also randomize the order of the photos at the same time
+    //Only add a random amount of firestore photos to the combination
+    const photos =
+      count > 20
+        ? [
+            ...unsplashPhotos,
+            ...ourPhotos
+              .sort(() => Math.random() - 0.5)
+              .slice(0, Math.floor(Math.random() * 15)),
+          ]
+        : unsplashPhotos;
+
+    res.status(200).json(photos);
   } catch (err: any) {
+    console.error(err);
     res.status(500).json({ error: err.message });
   }
 }
